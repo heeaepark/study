ed4e8198e0076eb1b7cde0dc3410fbb6ca5fe0dd
heeaepark <twins_0611@naver.com>
2022-09-14 23:00:10 +0900

    lifecycle / useEffect 스터디

--- react/shop/src/pages/Detail.js
+++ react/shop/src/pages/Detail.js
@@ -1,26 +1,58 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router"
 import styled from 'styled-components'
 
 const Detail = function(props) {
+    /* let alertDisplay = () => {
+        const alertBox = document.querySelector('.alert-warning');
+        setTimeout(()=> {
+            alertBox.style.display = 'none';
+        }, 2000);
+    } */
+    
     let {id} = useParams();
     let findProd = props.shoes.find(function(el){
         return el.id == id;
     });
-
-    let Custombtn = styled.button`
-        border: none;
-        background: ${props => props.bg};
-        color: ${props => props.bg == 'blue' ? 'white' : 'black'};
-    `
+    let [alert, setAlert] = useState(true);
+    let [checkAlert, setChekAlert] = useState(false);
+    let [inputValue, setInputValue] = useState('');
+    useEffect(() => {
+        /* alertDisplay(); */
+        const a = setTimeout(()=> {setAlert(false);}, 2000);
+        return () => {
+            clearTimeout(a)
+        }
+    }, []);
+    useEffect(()=> {
+        if(isNaN(inputValue) == true) {
+            setChekAlert(true);
+        } else {
+            setChekAlert(false);
+        }
+    }, [inputValue]);
+    
     return(
         <div className="container">
-            <Custombtn bg="blue">버튼입니당!</Custombtn>
+            {
+                alert == true ? 
+                <div className="alert alert-warning">2초 이내 구매시 할인!</div>
+                : null
+            }
             <div className="row">
                 <div className="col-md-6">
                     <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
                 </div>
                 <div className="col-md-6">
+                    {
+                        checkAlert == true ?
+                        <div className="alert alert-danger">숫자만 입력하세요</div>
+                        : null
+
+                    }
+                    <input type="text" onChange={(e)=> {
+                        setInputValue(e.target.value);
+                    }}/>
                     <h4 className="pt-5">{findProd.title}</h4>
                     <p>{findProd.content}</p>
                     <p>{findProd.price}</p>
