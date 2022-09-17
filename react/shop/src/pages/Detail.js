95fefefdc0005db3c0a0dac005c79e034539462d
heeaepark <twins_0611@naver.com>
2022-09-17 16:18:37 +0900

    탭 UI에 애니메이션 주기 (automatic batching 하지 않는 법)

--- react/shop/src/pages/Detail.js
+++ react/shop/src/pages/Detail.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router"
-import styled from 'styled-components'
+import styled from 'styled-components';
+import {Nav} from 'react-bootstrap';
+import './../App.css';
 
 const Detail = function(props) {
     /* let alertDisplay = () => {
@@ -17,50 +19,104 @@ const Detail = function(props) {
     let [alert, setAlert] = useState(true);
     let [checkAlert, setChekAlert] = useState(false);
     let [inputValue, setInputValue] = useState('');
+    let [tab, setTab] = useState(0);
+    let [fade2, setFade2] = useState('');
+
     useEffect(() => {
-        /* alertDisplay(); */
-        const a = setTimeout(()=> {setAlert(false);}, 2000);
-        return () => {
-            clearTimeout(a)
-        }
+      /* alertDisplay(); */
+      const a = setTimeout(()=> {setAlert(false);}, 2000);
+      return () => {
+          clearTimeout(a)
+      }
     }, []);
     useEffect(()=> {
-        if(isNaN(inputValue) == true) {
-            setChekAlert(true);
-        } else {
-            setChekAlert(false);
-        }
+      if(isNaN(inputValue) == true) {
+          setChekAlert(true);
+      } else {
+          setChekAlert(false);
+      }
     }, [inputValue]);
-    
+    useEffect(()=> {
+      setTimeout(() => {
+        setFade2('end');
+      }, 100);
+      return(
+        setFade2('')
+      )
+    }, []);
     return(
-        <div className="container">
+      <div className={`container start ${fade2}`}>
+        {
+            alert == true ? 
+            <div className="alert alert-warning">2초 이내 구매시 할인!</div>
+            : null
+        }
+        <div className="row">
+          <div className="col-md-6">
+              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+          </div>
+          <div className="col-md-6">
             {
-                alert == true ? 
-                <div className="alert alert-warning">2초 이내 구매시 할인!</div>
-                : null
+              checkAlert == true ?
+              <div className="alert alert-danger">숫자만 입력하세요</div>
+              : null
             }
-            <div className="row">
-                <div className="col-md-6">
-                    <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-                </div>
-                <div className="col-md-6">
-                    {
-                        checkAlert == true ?
-                        <div className="alert alert-danger">숫자만 입력하세요</div>
-                        : null
-
-                    }
-                    <input type="text" onChange={(e)=> {
-                        setInputValue(e.target.value);
-                    }}/>
-                    <h4 className="pt-5">{findProd.title}</h4>
-                    <p>{findProd.content}</p>
-                    <p>{findProd.price}</p>
-                    <button className="btn btn-danger">주문하기</button> 
-                </div>
-            </div>
+            <input type="text" onChange={(e)=> {
+              setInputValue(e.target.value);
+            }}/>
+            <h4 className="pt-5">{findProd.title}</h4>
+            <p>{findProd.content}</p>
+            <p>{findProd.price}</p>
+            <button className="btn btn-danger">주문하기</button> 
+          </div>
         </div>
+        <div className="row">
+          <div className="col-md-12">
+            <Nav variant="tabs"  defaultActiveKey="link0">
+              <Nav.Item>
+                <Nav.Link onClick={ ()=>{setTab(0)} } eventKey="link0">버튼0</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link onClick={ ()=>{setTab(1)} } eventKey="link1">버튼1</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link onClick={ ()=>{setTab(2)} } eventKey="link2">버튼2</Nav.Link>
+              </Nav.Item>
+            </Nav>
+            <TabContent tab={tab} />
+          </div>
+        </div>
+      </div>
+        
     )
+}
+
+function TabContent({tab, shoes}){/* props 대신 {}괄호 안에 useState 변수 넣어줘도 됨 */
+  /* if(tab == 0) {
+    return (<div>탭 01</div>);
+  }
+  if(tab == 1) {
+    return (<div>탭 02</div>);
+  }
+  if(tab == 2) {
+    return (<div>탭 03</div>);
+  } */
+
+  let [fade, setFade] = useState('');
+  useEffect(()=>{
+    setTimeout(()=> {
+      setFade('end');
+    }, 100)
+    return () => {
+      setFade('');
+    }
+  },[tab]);
+
+  return(
+    <div className={`start ${fade}`}>
+      {[<div>탭 01</div>, <div>탭 02</div>, <div>탭 03</div>][tab]}
+    </div>
+  )
 }
 
 export {Detail}
