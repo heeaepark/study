59a15d1db7e73829918998e8f0484f9a7c24d9c2
heeaepark <twins_0611@naver.com>
2022-09-21 20:55:41 +0900

    최근 본 상품 조건문 수정

--- react/shop/src/component/RecentlyItems.js
+++ react/shop/src/component/RecentlyItems.js
@@ -4,16 +4,18 @@ export default function RecentlyItem (props){
   let navigate = useNavigate();
   let recentlyStorage = JSON.parse(localStorage.getItem('watched'));
   let shoesList = props.shoes;
-      
+
   if(localStorage.getItem('watched') != null) {
-    recentlyStorage.map((a, i)=>{
-      return(
-        <div className="recently-item" key={i}>
-          <img src={`https://codingapple1.github.io/shop/shoes${a+1}.jpg`} onClick={()=>{ navigate('/detail/'+a) }} width="80%"/>
-          <p>{shoesList[a].title}</p>
-        </div>
-      )
-    })
+    return (
+      recentlyStorage.map((a, i)=>{
+        return(
+          <div className="recently-item" key={i}>
+            <img src={`https://codingapple1.github.io/shop/shoes${a+1}.jpg`} onClick={()=>{ navigate('/detail/'+a) }} width="80%"/>
+            <p>{shoesList[a].title}</p>
+          </div>
+        )
+      })
+    )
   }
       
 
