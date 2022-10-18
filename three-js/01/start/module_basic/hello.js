50591dac7a69dafed9f5110a15fea9734bf5cba2
heeaepark <pha060611@gmail.com>
2022-10-18 16:50:58 +0900

    my first three js study

---
+++ three-js/01/start/module_basic/hello.js
@@ -0,0 +1,6 @@
+export function hello1() {
+	console.log('Hello Three.js World!');
+}
+export function hello2() {
+	console.log('First Three.js project');
+}
---
+++ three-js/01/start/module_basic/main.js
@@ -0,0 +1,8 @@
+import { hello1, hello2 } from './hello.js';
+import * as hi from './hello.js';
+
+hello1();
+hello2();
+
+hi.hello1();
+hi.hello2();
